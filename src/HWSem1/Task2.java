56ec9f2e08a10b3700cd2b5189e3ee920b8f0b08
A9219 <alexey1978malyshev@gmail.com>
2023-06-27 16:21:09 +0300

    task2 added

--- src/HWSem1/Task2.java
+++ src/HWSem1/Task2.java
@@ -1,7 +1,7 @@
 package HWSem1;
 
 /*
-    2) Вывести все простые числа от 1 до 1000
+2) Вывести все простые числа от 1 до 1000
 3) Реализовать простой калькулятор
 4) (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0.
 Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69.
@@ -9,5 +9,22 @@ package HWSem1;
 */
 
 public class Task2 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 1000) {
+            boolean flag = true;
+            int j = 3;
+            while (j <= i/2) {
+                if (i % j == 0) {
+                    flag = false;
+                }
+                j++;
+            }
+            if (flag) System.out.println(i);
+            i++;
+
+        }
+    }
+
 
 }
