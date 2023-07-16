f4a48c12ea1aa9c49135d740552179144a998309
A9219 <alexey1978malyshev@gmail.com>
2023-07-16 22:03:33 +0300

    HW6 Store.fillNbStore updated

--- src/HWSem6/Store.java
+++ src/HWSem6/Store.java
@@ -11,13 +11,13 @@ public class Store {
         for (int i = 0; i < setSize; i++) {
             NoteBook unit = new NoteBook();
             nbSet.add(unit);
-            unit.brand = unit.brandNames[(int)(Math.random()*4)];
-            unit.system = unit.systemNames[(int)(Math.random()*5)];
-            unit.hdd = unit.hddValues[(int)(Math.random()*4)];
-            unit.ram = unit.ramValues[(int)(Math.random()*4)];
+            unit.brand = unit.brandNames[(int)(Math.random()*unit.brandNames.length)];
+            unit.system = unit.systemNames[(int)(Math.random()*unit.systemNames.length)];
+            unit.hdd = unit.hddValues[(int)(Math.random()*unit.hddValues.length)];
+            unit.ram = unit.ramValues[(int)(Math.random()*unit.ramValues.length)];
             unit.imei = unit.imeiValues;
-            unit.color = unit.colors[(int)(Math.random()*4)];
-            unit.screen = unit.screens[(int)(Math.random()*5)];
+            unit.color = unit.colors[(int)(Math.random()*unit.colors.length)];
+            unit.screen = unit.screens[(int)(Math.random()*unit.screens.length)];
 
         }
         System.out.printf("В магазине %d ноутбуков", nbSet.size());
